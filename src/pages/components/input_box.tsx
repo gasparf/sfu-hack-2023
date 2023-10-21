efdf66abe1eac53545882de98d1f81fd0b817608
Gaspar <gaspar.tan1189@gmail.com>
2023-10-21 13:18:18 -0700

    cleaning up unecessary code

--- src/pages/components/input_box.tsx
+++ src/pages/components/input_box.tsx
@@ -5,9 +5,9 @@ import TextSubmit from "./text_Submit"
 
 export default function InputBox() {
     return (
-        <div className="inputContainer flex  items-center p-0 w-full">
+        <div className="inputContainer flex w-full">
             <div className="inputBoxContainer p-1 flex-1 align-middle rounded-xl">
-                <input  type="text" placeholder="Your Input Goes Here..." className="userInput flex w-full justify-center self-start p-5 bg-beige rounded-xl text-black border border-black rounded-md">
+                <input  type="text" placeholder="Your Input Goes Here..." className="userInput flex w-full p-5 rounded-xl text-black border border-black ">
                 
                 </input>
             </div>
