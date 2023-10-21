3dbc2c71c5bbe94bc9f78bd25604f046cacbc750
Gaspar <gaspar.tan1189@gmail.com>
2023-10-21 13:12:36 -0700

    fixed input container & submit button

--- src/pages/components/input_box.tsx
+++ src/pages/components/input_box.tsx
@@ -5,9 +5,9 @@ import TextSubmit from "./text_Submit"
 
 export default function InputBox() {
     return (
-        <div className="inputContainer flex items-center justify-center p-0">
-            <div className="inputBoxContainer p-1 align-middle rounded-xl">
-                <input  type="text" placeholder="Your Input Goes Here..." className="userInput flex flex-1 justify-center self-start w-full p-5 bg-beige rounded-xl text-black border border-black rounded-md">
+        <div className="inputContainer flex  items-center p-0 w-full">
+            <div className="inputBoxContainer p-1 flex-1 align-middle rounded-xl">
+                <input  type="text" placeholder="Your Input Goes Here..." className="userInput flex w-full justify-center self-start p-5 bg-beige rounded-xl text-black border border-black rounded-md">
                 
                 </input>
             </div>
